b60c07771dd20695986cdd3d9aea2b0b932056fe
Matheus Fidelis <mfds@a.recife.ifpe.edu.br>
2020-11-06 15:20:55 -0300

    Primeiro commit com todos os arquivos de aprendizagem até o fim do primeiro módulo

---
+++ Readme.md
@@ -0,0 +1,6 @@
+#Projetos Java Aprendizagem
+Aluno: Matheus Fidelis da Silva.
+email: mfds@a.recife.ifpe.edu.br
+
+Projetos e atividades referentes ao curso Java do zero ao avançado.
+
---
+++ testedata/src/testedata/TesteData.java
@@ -0,0 +1,11 @@
+package testedata;
+
+import java.util.Date;
+
+public class TesteData {
+	public static void main(String[] args) {
+		Date d = new Date();
+		
+		System.out.println(d);
+	}
+}
